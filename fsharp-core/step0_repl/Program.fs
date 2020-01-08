c710e3b9efebea7d3779fc1a23c82c2522d82bdd
David Glassborow <dave@conceptfirst.com>
2020-01-08 13:51:45 +0000

    step0 files

---
+++ fsharp-core/step0_repl/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+open Mono.Terminal
+
+
+[<EntryPoint>]
+let main argv =
+
+    let le = LineEditor("mal", HeuristicsMode = "lisp")
+
+    let mutable complete = false
+    while not complete do
+        let line = le.Edit("user> ", "")
+        if isNull line then complete <- true
+        else
+            printfn "%s" line
+
+    printfn "-Done-"
+    0 // return an integer exit code
